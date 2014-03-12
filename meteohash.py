8b7894a15a614596702489d0be874f9fda681718
Fabio Rueda <avances123@gmail.com>
2014-03-13 00:05:02 +0100

    midiendo con time()

--- meteohash.py
+++ meteohash.py
@@ -1,18 +1,12 @@
-import cProfile
-import pstats
 import pandas as pd
 import sys
+import time
 
     
 
-def lectura(d):
-    """
-    FUNCTION TO BENCHMARK: dict scan
-    """  
-    nodos = []
+def dict_scan(d):
     for key in d.keys():
-        nodos.append(d[key])
-    return nodos
+        nodo = d[key]
 
 
 
@@ -21,29 +15,24 @@ if __name__ == '__main__':
     # Leo el csv
     raw_data = pd.io.parsers.read_csv(sys.argv[1],compression='gzip',sep="|",nrows=None)
     print("Cargado raw_data, shape:",raw_data.shape)
-    # Creo una columna de strings y otra de enteros concatenando, son nuestras keys (no son unicas)
+    # Creo una columna de strings y otra de enteros, son nuestras keys (no son unicas, hay 875684 unicas)
     data = pd.DataFrame()
-    data['string'] = raw_data['id_modelo'].apply(str) + raw_data['version'].apply(str) + raw_data['variable'].apply(str) + raw_data['nivel'].apply(str) + raw_data['pasada'].apply(str)
-    data['integer'] = data['string'].apply(int)
+    data['strings'] = raw_data['id_modelo'].apply(str) + raw_data['version'].apply(str) + raw_data['variable'].apply(str) + raw_data['nivel'].apply(str) + raw_data['pasada'].apply(str)
+    data['integers'] = data['strings'].apply(int)
 
     
     # Meto las keys y los values en los diccionarios
-    dict_s = dict(zip(data.string , raw_data.id_nodo))
-    print("Cargado dict de strings, numero de claves:",len(dict_s))
-    dict_i = dict(zip(data.integer, raw_data.id_nodo))
-    print("Cargado dict de integers, numero de claves:",len(dict_i))
     # TODO: Si queremos guardar todos los nodos para una key: http://stackoverflow.com/a/6795385/472866
+    dict_s = dict(zip(data.strings , raw_data.id_nodo))
+    dict_i = dict(zip(data.integers, raw_data.id_nodo))
+    
 
 
-    nodos = []
-    cProfile.run("nodos = lectura(dict_s)",'.stats_lectura_s')
-    r_stats = pstats.Stats('.stats_lectura_s')
-    r_stats.print_stats()
-    print("longitud resultado lista con los nodos:",len(nodos))
+    start_time = time.time()
+    nodos = dict_scan(dict_i)
+    stop_time = time.time()
+    print(stop_time - start_time, "seconds")
+    
 
-    nodos = []
-    cProfile.run("nodos = lectura(dict_s)",'.stats_lectura_i')
-    r_stats = pstats.Stats('.stats_lectura_i')
-    r_stats.print_stats()
-    print("longitud resultado lista con los nodos:",len(nodos))    
+
     
